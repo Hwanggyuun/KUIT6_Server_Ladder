8b767940b5c34d41734f729de57ab2c1b9fb763b
jyun27 <kimjyun27@gmail.com>
2025-09-18 06:56:17 +0900

    add: 1week-basic Ladder

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,8 +1,22 @@
 public class Ladder {
 
-    private final int[][] rows;
+    private final Row[] rows;
 
     public Ladder(int row, int numberOfPerson) {
-        rows = new int[row][numberOfPerson];
+        rows = new Row[numberOfPerson];
+        for (int i = 0; i < numberOfPerson; i++) {
+            rows[i] = new Row(numberOfPerson);
+        }
+    }
+
+    public void drawLine(int row, int col){
+        rows[row].drawLine(col);
+    }
+
+    public int run(int position){
+        for(int i=0; i<rows.length; i++){
+            position = rows[i].nextPosition(position);
+        }
+        return position;
     }
 }
---
+++ src/main/java/Row.java
@@ -0,0 +1,53 @@
+public class Row {
+    private final int[] row;
+
+    public Row(int numberOfPerson){
+        validateNumberPerson(numberOfPerson);
+        row = new int[numberOfPerson];
+    }
+
+    public int nextPosition(int position){
+        validatePosition(position);
+
+        if(isRight(position)){
+            return position + 1;
+        }
+
+        if(isLeft(position)){
+            return position - 1;
+        }
+        return position;
+    }
+
+    private boolean isLeft(int position) {
+        return row[position] == -1;
+    }
+
+    private boolean isRight(int position) {
+        return row[position] == 1;
+    }
+
+    private void validatePosition(int position) {
+        if(position >= row.length || position < 0){
+            throw new IllegalArgumentException("유효하지 않은 위치입니다.");
+        }
+    }
+
+    public void drawLine(int startPosition){
+        validateDrawLinePosition(startPosition);
+        row[startPosition] = 1;
+        row[startPosition + 1] = -1;
+    }
+
+    private void validateDrawLinePosition(int startPosition) {
+        if (startPosition >= row.length -1 || startPosition < 0 || row[startPosition] == -1 || row[startPosition + 1] == 1) {
+            throw new IllegalArgumentException("사다리를 그릴 수 없는 위치입니다.");
+        }
+    }
+
+    private void validateNumberPerson(int numberOfPerson) {
+        if(numberOfPerson <= 1){
+            throw new IllegalArgumentException("참여 인원은 1명 이상이어야 합니다.");
+        }
+    }
+}
