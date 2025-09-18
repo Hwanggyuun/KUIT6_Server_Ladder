dfcae2803b2937ba1b84c140c188dade016ac747
hwanggyuun <gka365@naver.com>
2025-09-18 23:51:31 +0900

    feat: 사다리 클래스 구현

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,8 +1,22 @@
 public class Ladder {
 
-    private final int[][] rows;
-
+    private final Rows rows;
     public Ladder(int row, int numberOfPerson) {
-        rows = new int[row][numberOfPerson];
+        this.rows = new Rows(row, numberOfPerson);
     }
+
+    public int run(SelectedIndex S){
+        Index index = new Index(S);
+        for (int[] ints : rows.all()) {
+            int i = index.readValue(ints);
+            Index.checkIndex(i, index);
+        }
+        return index.getResult();
+    }
+
+    public void drawLine(int a, int b, Height height) {
+        rows.draw(a,b,height);
+    }
+
+
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,5 +1,90 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+
+    @Test
+    @DisplayName("사다리 내려가는거")
+    public void LadderRun () {
+        //given
+        Ladder ladder = new Ladder(3,3);
+
+        //when
+        assertThat(ladder.run(new SelectedIndex(2))).isEqualTo(2);
+
+
+    }
+    @Test
+    @DisplayName("선 하나 그리고 하는거")
+    public void LadderDrawLine () {
+        //given
+        Ladder ladder = new Ladder(5,4);
+        ladder.drawLine(2,3,new Height(2));
+        //when
+        assertThat(ladder.run(new SelectedIndex(2))).isEqualTo(3);
+    }
+    @Test
+    @DisplayName("길이를 넘었습니다")
+    public void throwLine() throws Exception {
+        //given
+        Ladder ladder = new Ladder(5,4);
+        assertThatThrownBy(() -> ladder.drawLine(2,3,new Height(6)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        //when
+
+    }
+    @Test
+    @DisplayName("하나 차이나게 선을 그어주세요")
+    public void throwSameLine() throws Exception {
+        //given
+        Ladder ladder = new Ladder(5,4);
+        assertThatThrownBy(() -> ladder.drawLine(3,3,new Height(4)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        //when
+
+    }
+    @Test
+    @DisplayName("두 칸 이상은 선을 그을 수 없습니다")
+    public void throwDoubleLine() throws Exception {
+        //given
+        Ladder ladder = new Ladder(5,4);
+        assertThatThrownBy(() -> ladder.drawLine(1,3,new Height(4)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        //when
+
+    }
+
+    @Test
+    @DisplayName("같은 칸에 선을 그을 수 없습니다")
+    public void throwSameLines() throws Exception {
+        //given
+        Ladder ladder = new Ladder(5,4);
+        ladder.drawLine(2,3,new Height(3));
+        assertThatThrownBy(() -> ladder.drawLine(2,3,new Height(3)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        //when
+
+    }
+    @Test
+    @DisplayName("옆 칸에 선을 그을 수 없습니다")
+    public void throwNextLines() throws Exception {
+        //given
+        Ladder ladder = new Ladder(5,4);
+        ladder.drawLine(1,2,new Height(4));
+        assertThatThrownBy(() -> ladder.drawLine(2,3,new Height(4)))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        //when
+
+    }
 
 }
