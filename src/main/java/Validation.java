1d0ed6dc83f0b962b4f6de0ac6031101627c7899
hwanggyuun <gka365@naver.com>
2025-09-23 16:56:41 +0900

    feat: 검증 클래스 추가

---
+++ src/main/java/Validation.java
@@ -0,0 +1,19 @@
+public class Validation {
+    public static String RowsValidation(Row[] rows, int leftColumn, int rightColumn, Height height){
+        int min = Math.min(leftColumn, rightColumn) - 1;
+        int max = Math.max(leftColumn, rightColumn) - 1;
+
+        if(Math.abs(leftColumn-rightColumn)!=1) {
+            return ErrorMessage.NOT_ADJACENT.getMessage();
+        }
+        if(height.current()>rows.length) {
+            return ErrorMessage.OUT_OF_HEIGHT.getMessage();
+        }
+        if (leftColumn == rightColumn) return ErrorMessage.SAME_COLUMN.getMessage();
+        if (rows[height.current()].line()[min] != 0 || rows[height.current()].line()[max] != 0) return ErrorMessage.SAME_COLUMN.getMessage();
+        if (min - 1 >= 0 && rows[height.current()].line()[min - 1] == Rows.RIGHT) return ErrorMessage.SAME_COLUMN.getMessage();
+        if (max + 1 < rows[0].length() && rows[height.current()].line()[max + 1] == Rows.LEFT) return ErrorMessage.SAME_COLUMN.getMessage();
+
+        return null;
+    }
+}
