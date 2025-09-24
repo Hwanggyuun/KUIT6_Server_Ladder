1e31b5d21c69b1d4a434c69cf2dce970321bef39
hwanggyuun <gka365@naver.com>
2025-09-24 13:03:03 +0900

    fix: direction 으로 변경

--- src/main/java/CheckIndex.java
+++ src/main/java/CheckIndex.java
@@ -2,11 +2,11 @@ public class CheckIndex {
     private CheckIndex() {} // 인스턴스화 방지
 
     public static void checkIndex(int i, Index index) {
-        if (i == Rows.LEFT) {
+        if (i == Direction.LEFT.getValue()) {
             index.sub();
             return;
         }
-        if (i == Rows.RIGHT) {
+        if (i == Direction.RIGHT.getValue()) {
             index.add();
             return;
         }
