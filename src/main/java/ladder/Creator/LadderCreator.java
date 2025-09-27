e0df841b2927ebc53be510144fec56439eebaabb
hwanggyuun <gka365@naver.com>
2025-09-27 17:04:47 +0900

    feat: ladderCreator factory pattern 이용하기

--- src/main/java/ladder/Creator/LadderCreator.java
+++ src/main/java/ladder/Creator/LadderCreator.java
@@ -1,25 +1,10 @@
 package ladder.Creator;
 
-import ladder.Domain.GreaterThanOne;
-import ladder.Domain.Position;
 import ladder.Domain.Row;
+import ladder.Domain.Position;
 
-public class LadderCreator {
-
-    private final Row[] rows;
-
-    public LadderCreator(GreaterThanOne numberOfRows, GreaterThanOne numberOfPerson) {
-        rows = new Row[numberOfRows.getNumber()];
-        for (int i = 0; i < numberOfRows.getNumber(); i++) {
-            rows[i] = new Row(numberOfPerson);
-        }
-    }
-
-    public Row[] getRows() {
-        return rows;
-    }
-
-    public void drawLine(Position row, Position col){
-        rows[row.getValue()].drawLine(col);
-    }
+public interface LadderCreator {
+    Row[] getRows();
+    void drawLine(Position row, Position col);
 }
+
